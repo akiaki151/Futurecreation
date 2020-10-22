35db7a7b04d1d8f20b16c775fe3c72612b2604ec
makimaki <makimaki.akiaki275@gmail.com>
2020-10-22 11:38:35 +0900

    Revert "エンジンScene追加"
    This reverts commit 09e8fe754db0c4cb3984763fcbb37e20c6dc98d5.

--- Assets/Text/Scripts/Actions.cs
+++ Assets/Text/Scripts/Actions.cs
@@ -1,16 +1,42 @@
 ﻿public class Actions
 {
     GameController gc;
-    
+
     public Actions(GameController gc)
     {
         this.gc = gc;
     }
 
     //行いたい処理などを書いていく
-    public void Test()
+    public void SelectAction(int index)
     {
-        gc.Sc.SetScene("004");
+        switch (index)
+        {
+            case 0:
+                //gc.Sc.SetScene("004");
+                break;
+            case 1:
+                gc.Sc.ActionMove();
+                break;
+            case 2:
+                gc.Sc.SetScene("004");
+                break;
+            case 3:
+                gc.Sc.SetScene("004");
+                break;
+            case 4:
+                gc.Sc.SetScene("004");
+                break;
+            case 5:
+                gc.Sc.SetScene("004");
+                break;
+            default:
+                break;
+        }
     }
 
+    public void Updata()
+    {
+
+    }
 }

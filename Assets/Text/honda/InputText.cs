8779a3f615683bc03420c8ea8dd6b9dd188224ec
bookman0156 <donaku15@gmail.com>
2020-10-30 15:05:10 +0900

    Revert "キャラ、アイコン、発言者、背景、Log追加"
    This reverts commit 3668c4f648e3bae031c7a276fa56ecb3a21dcd03.

--- Assets/Text/honda/InputText.cs
+++ Assets/Text/honda/InputText.cs
@@ -6,22 +6,15 @@ using System.IO;
 
 public class InputText : MonoBehaviour
 {
-    const int line_size = 500;
     InputField m_InputText;
-    GameObject[] iconselectbutton = new GameObject[50];
     public Dropdown m_dropdown;
     public Dropdown m_CharName;
-    public Button m_CharImage;
-    public Button m_IconImage;
-    public Image m_BgImage;
-    public GameObject m_ImageBar;
-    public GameObject m_Log;
-    //private List<string> csvData = new List<string>();
-    private string[] csvData = new string[line_size];
+    public Dropdown m_CharImage;
+    private List<string> csvData = new List<string>();
     private string path;
     private string fileName = "Data/TextData.csv";
     private string stock;
-    private int filecount = 0;
+    public Button m_button;
 
     // Start is called before the first frame update
     void Start()
@@ -32,17 +25,19 @@ public class InputText : MonoBehaviour
         InitData();
 
     }
+
     //InputFieldの初期化
     void InitData()
     {
-        m_InputText.text = "";
-        stock = "";
+        m_InputText.text = " ";
+        stock = " ";
     }
 
     //文字列保存
     public void InputData()
     {
         string TextData = m_InputText.text;
+        //Debug.Log(TextData);
 
         FileData(TextData, m_dropdown.value + 1);
 
@@ -59,10 +54,9 @@ public class InputText : MonoBehaviour
 
         //変更された情報の書き出し
         StreamWriter sw = new StreamWriter(path, false, Encoding.GetEncoding("Shift_JIS"));
-        for (int i = 0;i < line_size; i++)
+        for (int i = 0;i < 500;i++)
         {
             sw.WriteLine(csvData[i]);
-            Debug.Log(csvData[i]);
             //Debug.Log(csvData[i]);
         }
         sw.Close();
@@ -79,10 +73,11 @@ public class InputText : MonoBehaviour
         while (sr.Peek() > -1)
         {
             string line = sr.ReadLine();
-            csvData[index] = line;
+            csvData.Add(line);
+            //Debug.Log(csvData[index]);
             index++;
         }
-         sr.Close();
+        sr.Close();
     }
 
     public void SpeakerName()
@@ -90,117 +85,36 @@ public class InputText : MonoBehaviour
         m_InputText.text = stock + "#speaker=" + m_CharName.options[m_CharName.value].text;
         stock = m_InputText.text;
     }
+    //public void DeleteSpeakerName()
+    //{
+    //    m_InputText.text = stock + "#speaker=" + m_CharName.options[m_CharName.value].text;
+    //    stock = m_InputText.text;
+    //}
+
+
     public void CharaImage()
     {
-        m_InputText.text = stock + "#chara=" + m_CharImage.image.sprite.name;
+        m_InputText.text = stock + "#chara=" + m_CharImage.captionImage.sprite.name;
         stock = m_InputText.text;
-        DeleteButton();
     }
     public void DeleteCharaImage()
     {
         m_InputText.text = stock + "#chara=del_chara";
         stock = m_InputText.text;
-        m_CharImage.image.sprite = null;
     }
-    public void IconImage()
+
+    public void Sarchfolder()
     {
-        m_InputText.text = stock + "#ico=" + m_IconImage.image.sprite.name;
-        stock = m_InputText.text;
-        DeleteButton();
-    }
-    public void DeleteIconImage()
-    {
-        m_InputText.text = stock + "#ico=del_chara";
-        stock = m_InputText.text;
-        m_IconImage.image.sprite = null;
-    }
-    public void BackImage()
-    {
-        m_InputText.text = stock + "#background=" + m_BgImage.sprite.name;
-        stock = m_InputText.text;
-        DeleteButton();
-    }
-    public void OpenBar()
-    {
-        if (!m_ImageBar.activeSelf)
-            m_ImageBar.SetActive(true);
-    }
-    public void OpenBackLog()
-    {
-        if (!m_Log.activeSelf)
-            m_Log.SetActive(true);
-    }
-    public void Sarchiconfolder()
-    {
-        GameObject obj = Resources.Load<GameObject>("hondaPrefab/IConSelectButton");
-        GameObject parent = GameObject.Find("Content");
+        //Image img = GetComponent<Image>();
         DirectoryInfo directory = new DirectoryInfo("Assets/Text/Resources/Image/CharaIcons/");
         FileInfo[] files = directory.GetFiles("*.png");
-        string[] afterFile = new string[files.Length];
-        filecount = files.Length;
-        if (parent.transform.childCount == 0)
-        {
-            for (int i = 0; i < files.Length; i++)
-            {
-                afterFile[i] = files[i].Name.Replace(".png", "");
-                Sprite sprite = Resources.Load<Sprite>("Image/CharaIcons/" + afterFile[i]);
-                iconselectbutton[i] = Instantiate(obj) as GameObject;
-                iconselectbutton[i].GetComponent<Button>().image.sprite = sprite;
-                iconselectbutton[i].transform.SetParent(parent.transform, false);
-            }
-        }
-    }
-    public void SarchCharcterfolder()
-    {
-        GameObject obj = Resources.Load<GameObject>("hondaPrefab/CharacterSelectButton");
-        GameObject parent = GameObject.Find("Content");
-        DirectoryInfo directory = new DirectoryInfo("Assets/Text/Resources/Image/Characters/");
-        FileInfo[] files = directory.GetFiles("*.png");
-        string[] afterFile = new string[files.Length];
-        filecount = files.Length;
-        if (parent.transform.childCount == 0)
-        {
-            for (int i = 0; i < files.Length; i++)
-            {
-                afterFile[i] = files[i].Name.Replace(".png", "");
-                Sprite sprite = Resources.Load<Sprite>("Image/Characters/" + afterFile[i]);
-                iconselectbutton[i] = Instantiate(obj) as GameObject;
-                iconselectbutton[i].GetComponent<Button>().image.sprite = sprite;
-                iconselectbutton[i].transform.SetParent(parent.transform, false);
-            }
-        }
-    }
-    public void SarchBackImagefolder()
-    {
-        GameObject obj = Resources.Load<GameObject>("hondaPrefab/BackimgSelectButton");
-        GameObject parent = GameObject.Find("Content");
-        DirectoryInfo directory = new DirectoryInfo("Assets/Text/Resources/Image/BackGrounds/");
-        FileInfo[] files = directory.GetFiles("*.jpg");
-        string[] afterFile = new string[files.Length];
-        filecount = files.Length;
-        if (parent.transform.childCount == 0)
-        {
-            for (int i = 0; i < files.Length; i++)
-            {
-                afterFile[i] = files[i].Name.Replace(".jpg", "");
-                Sprite sprite = Resources.Load<Sprite>("Image/BackGrounds/" + afterFile[i]);
-                iconselectbutton[i] = Instantiate(obj) as GameObject;
-                iconselectbutton[i].GetComponent<Button>().image.sprite = sprite;
-                iconselectbutton[i].transform.SetParent(parent.transform, false);
-            }
-        }
-    }
-    public void DeleteButton()
-    {
-        for (int i = 0; i < filecount; i++)
-        {
-            Debug.Log(iconselectbutton);
-            Destroy(iconselectbutton[i]);
-        }
-        m_ImageBar.SetActive(false);
-    }
-    public void CloseLog()
-    {
-        m_Log.SetActive(false);
+        //foreach(FileInfo f in files)
+        //{
+        //    Sprite sprite = Resources.Load<Sprite>("Image/CharaIcons/" + f.Name);
+        //    m_button.image.sprite = sprite;
+        //}
+        Sprite sprite = Resources.Load<Sprite>("Image/CharaIcons/" + files[0].Name);
+        m_button.image.sprite = sprite;
+        Debug.Log(sprite);
     }
 }

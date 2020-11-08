b81d7c68b9560c8621a5bc2feea5be151c22bd0d
bookman0156 <donaku15@gmail.com>
2020-11-08 20:02:22 +0900

    ちょっとタグ入力のところを修正

---
+++ Assets/Engin/honda/Engin_Const.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Const_e
+{
+    public class Engin_Const : MonoBehaviour
+    {
+        public const int line_size = 500;
+    }
+
+}

4a36cc38417c26ab4d95b9021a87be954eb7425a
TomomoN-8739 <tomoki1020a@gmail.com>
2020-11-08 19:33:18 +0900

    エラーが取れなかったので、とりあえず現状を上げます。 まだ何も動きません。

---
+++ Assets/TomomoN/Script/EditorObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorObject : MonoBehaviour
+{
+    public bool bDraw = true;         // ギズモの描画フラグ
+    public float radius = 2.5f;        // 大きさ
+    public Color color = Color.blue;   // 色
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!bDraw) return;
+
+        Gizmos.color = color;
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+}
---
+++ Assets/TomomoN/Script/RoutePoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RoutePoint
+{
+    // 移動先のTransform
+    public Transform Point;
+
+    /* ↓必要なものがあったら随時追加↓ */
+}
